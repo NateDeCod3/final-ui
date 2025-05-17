09ff0fac866f0a2a36bbe0de21e2002af8514762
NateDeCod3 <100500429+NateDeCod3@users.noreply.github.com>
2025-05-17 09:04:50 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -55,8 +55,7 @@ const Header = ({ toggleDarkMode, isDarkMode }) => {
                     color: isDarkMode ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.3)',
                     pointerEvents: 'none',
                 }}
-            >
-                Z
+            ></div>
             </div>
         </header>
     );
